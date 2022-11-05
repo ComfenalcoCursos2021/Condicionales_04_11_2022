b96795fd21164e94705aa46fd179d04a47df2970
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-11-04 19:19:01 -0500

    Condicionales switch

--- api.php
+++ api.php
@@ -1,16 +1,15 @@
 <?php
-    $edad = 1;
-    if($edad==0){
-        echo "Usuario usted esta muerto";
-    }else if($edad>=22){
-        echo "Puede entrar al casino ";
-        if($edad>=50 && $edad<=60){
-            echo "Usuario por su edad tiene un descuesto en nuestro casino";   
-        }
-    }else if($edad>=18){
-        echo "Usted es mayor de edad pero no puede entrar al casino";
-    }else{
-        echo "Usted es menor de edad ";
+    $nombre = "miguel";
+    switch (ucfirst(strtolower($nombre))) {
+        case 'Miguel':
+            echo "Bienvenido a netflix";
+            break;
+        case 'Juan':
+            echo "Bienvenido a netflix";
+            break;
+        default:
+            echo "Usuario no valido";
+            break;
     }
 
 ?>
