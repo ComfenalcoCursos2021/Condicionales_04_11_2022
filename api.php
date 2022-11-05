46697fc8de9073f135d5bb49aa296cd7fa62fa13
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-11-04 19:39:42 -0500

    Condicionales match

--- api.php
+++ api.php
@@ -1,15 +1,10 @@
 <?php
-    $nombre = "miguel";
-    switch (ucfirst(strtolower($nombre))) {
-        case 'Miguel':
-            echo "Bienvenido a netflix";
-            break;
-        case 'Juan':
-            echo "Bienvenido a netflix";
-            break;
-        default:
-            echo "Usuario no valido";
-            break;
-    }
-
+    $edad = 50;
+    $res = match(true){
+        ($edad >= 22 && $edad<50) => "Puede entrar al casino",
+        ($edad >= 50 && $edad<=60) => "Puede entrar al casino y tiene descuesto",
+        $edad >= 18 => "Usted es mayor de edad",
+        default => "Usuario no valido"
+    };
+    echo $res;
 ?>
